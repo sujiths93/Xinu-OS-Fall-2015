eba54be633d7058c05b2ac58b88520766ecac47f
Sujith <sujishiv@iu.edu>
2015-11-05 22:35:17 -0500

    seven

--- bbb-xinu/shell/xsh_len.c
+++ bbb-xinu/shell/xsh_len.c
@@ -1,25 +1,35 @@
 #include<xinu.h>
 shellcmd xsh_len(int args,char *argv[])
 {
-	
-	if(strncmp(argv[0],"len",3)==0)
-{
-	struct memblk *cur;
-	
-	cur=&memlist;
-	while(cur->mnext!=NULL)
+	if(argc==2)
 	{
-		cur=cur->mnext;
-		printf("Starting address is %d \n",cur);		
-		printf("Length of the heap is %u \n",cur->mlength);
+		if(strncmp(argv[0],"len",3)==0)
+	    	{
+			struct memblk *cur;
+	
+			cur=&memlist;
+			while(cur->mnext!=NULL)
+			{
+	               		cur=cur->mnext;
+				printf("Starting address is %d \n",cur);		
+				printf("Length of the heap is %u \n",cur->mlength);
 		
-	 }
+	 		}
 	
 
-}
-else
-{
+	   	}
+		if(strncmp(argv[1],"--help",6)==0)
+		{
+ 		printf("\n\nUse: %s -f \n\n", argv[0]);
+		printf("Description:\n");
+		printf("\t Enter flag for use of futures\n");
+		printf("\t--help\t display this help and exit\n");
+		return OK;
+
+		}
+	}
+
 	printf("ERROR");
-}
+
 return 0;
 }
--- bbb-xinu/shell/xsh_stk.c
+++ bbb-xinu/shell/xsh_stk.c
@@ -2,16 +2,28 @@
 int i;
 shellcmd xsh_stk(int argc ,char * argv[])
 {
+if (argc==2)
+	  {
 		if(strncmp(argv[0],"stk",3)==0)
-{
+		{
 	
-	resume(create(rec,1024,24,"pocess1",0));
-	sleep(1);
+		resume(create(rec,1024,24,"pocess1",0));
+		sleep(1);
+		return OK;
+		}
+		if(strncmp(argv[1],"--help",6)==0)
+		{
+ 		printf("\n\nUse: %s -f \n\n", argv[0]);
+		printf("Description:\n");
+		printf("\t Enter flag for use of futures\n");
+		printf("\t--help\t display this help and exit\n");
+		return OK;
+
+		}
 }
-else
-{
- printf("ERRO\n");
-}	
+
+ printf("ERROR\n");
+	
 	
 	
 return OK;
